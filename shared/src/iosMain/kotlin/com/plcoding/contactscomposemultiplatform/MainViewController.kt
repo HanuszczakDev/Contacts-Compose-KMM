8267f2054f6ec0d4806d1071c946b857a796df35
ahanuszczak <adam.hanuszczak@gmail.com>
2023-11-08 14:44:03 +0100

    ContactList - first run on emulator

---
+++ shared/src/iosMain/kotlin/com/plcoding/contactscomposemultiplatform/MainViewController.kt
@@ -0,0 +1,11 @@
+package com.plcoding.contactscomposemultiplatform
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController {
+    val isDarkTheme = UIScreen
+    App(
+        darkTheme = false,
+        dynamicColor = false
+    )
+}
